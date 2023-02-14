c391bc8a58ab872990f58f9a05541d76c01f7dba
Stepan163s <109583349+Stepan163s@users.noreply.github.com>
2023-02-14 22:27:41 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from yandex_music import Client
+
+
+client = Client('AQAAAAAuVIqNAAG8Xh_Jb6PkM0Shg3aEnokwMZk').init()
+
+
+queues = client.queues_list()
+last_queue = client.queue(queues[0].id)
+last_track_id = last_queue.get_current_track()
+last_track = last_track_id.fetch_track()
+artists = ', '.join(last_track.artists_name())
+title = last_track.title
+print(f'{artists} - {title}')
+
